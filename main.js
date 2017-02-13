a80db45cb8153c1fccb18c31de6d9ef61e4cfde7
@ReynierNR <reynier.nr@gmail.com>
2017-02-12 20:32:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main () {
+    
+    $('.menu_bar').click(function() {
+        
+        if (contador == 1) {
+              $('nav').animate ({ 
+               left:'0'
+            });
+            contador = 0;
+            } else {
+            
+            contador = 1;
+            $('nav').animate({ 
+               left:'-100%'
+            });
+            
+        }
+        
+    });
+    
+    
+   // Mostramos y Ocultamos submenus
+    $('.submenu').click(function(){
+        
+        $(this).children('.children').fadeIn();
+    });
+    
+}
